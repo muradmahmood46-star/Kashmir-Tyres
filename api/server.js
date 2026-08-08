08441863ba566b57d88d6b273b2d8b1176461236
muradmahmood46-star <muradmahmood46star@gmail.com>
2026-08-08 21:23:20 +0500

    fix: change api/server.js to valid ES module to prevent ReferenceError module is not defined

--- api/server.js
+++ api/server.js
@@ -1,1 +1,1 @@
-try { module.exports = require('../server/index.cjs') } catch (e) { module.exports = (req, res) => res.status(500).json({ error: 'Failed to boot', details: e.message, stack: e.stack }) }
+import app from '../server/index.cjs'; export default app;
